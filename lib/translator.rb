50d6fe3bfadb09cbf8497018fed2147c689a0026
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-22 23:49:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file)
   # code goes here
   library = YAML.load(File.read(file))
   library.each_with_object({}) do |(key,value), new_hash|
-    
+    new_hash[key]
   end
   binding.pry
   new_hash
