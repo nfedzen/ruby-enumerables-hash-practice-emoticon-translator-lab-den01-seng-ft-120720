34945aed45e76d265f988cd67067d128bf20f565
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-22 23:43:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
 # require modules here
-
-def load_library
+require 'yaml'
+require 'pry'
+def load_library(file)
   # code goes here
+  library = YAML.load(File.read(file))
+  library.each_with_object({}) do |(key,value), new_hash|
+    new_hash[key]
+    value.each do |emoji|
+      new_hash[key] = {}
+    end
+  end
+  #binding.pry
+  new_hash
 end
 
 def get_japanese_emoticon
