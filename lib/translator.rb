f1abcec202feba0359e62abfc1e89ee222dd82fb
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-22 23:59:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,9 +7,14 @@ def load_library(file)
   final_library = library.each_with_object({}) do |(key,value), new_hash|
     new_hash[key] = {:english => "", :japanese => ""}
     binding.pry
-    
+    value.each do |emoji|
+      if new_hash[key][:english] == ""
+        new_hash[key][:english] = emoji
+      else 
+        new_hash[key][:japanese] = emoji
+    end
   end
-  binding.pry
+  final_library
 end
 
 def get_japanese_emoticon
