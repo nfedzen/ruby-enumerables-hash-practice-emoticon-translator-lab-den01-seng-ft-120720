cfbd400f27d0b7f4fecd04d0857a156191d1200e
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-22 23:54:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,9 @@ def load_library(file)
   # code goes here
   library = YAML.load(File.read(file))
   final_library = library.each_with_object({}) do |(key,value), new_hash|
-    new_hash = 
+    new_hash[key] = {:english => "", :japanese => ""}
+    binding pry
+    
     
   end
   binding.pry
